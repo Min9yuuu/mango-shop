630ebb80875e8a9d63471881f6a28853db8d5e61
M9 <9yu6820@gmail.com>
2023-03-17 15:41:41 +0900

    서버 url 수정

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://port-0-mango-shop-server-p8xrq2mlfc5j4rt.sel3.cloudtype.app/";
+export const API_URL = "https://port-0-mango-shop-server-p8xrq2mlfc5j4rt.sel3.cloudtype.app";
